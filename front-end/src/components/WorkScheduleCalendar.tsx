b39aa4cd1da894af7940f0f32c2a45b95ccd4dbb
Mateusz Wende <matewen129@student.polsl.pl>
2020-03-21 12:32:35 +0100

    Comment recurring events in work schedule calendar - not needed

--- front-end/src/components/WorkScheduleCalendar.tsx
+++ front-end/src/components/WorkScheduleCalendar.tsx
@@ -25,30 +25,31 @@ const WorkScheduleCalendar: React.FC<WorkingScheduleCalendarPropsT> = ({
 }) => {
   const [eventsState, setEventsState] = useState<Event[]>(events);
 
-  const handleRecurringEvents = (nextWeek: Date) => {
-    let recurringEvents: Event[] = [];
-    if (eventsState.length > 0) {
-      if (nextWeek.getTime() > eventsState[0].start!.getTime()) {
-        recurringEvents = eventsState.map((event: Event) => {
-          return {
-            ...event,
-            start: addDays(event.start!, 7),
-            end: addDays(event.end!, 7)
-          };
-        });
-        setEventsState(recurringEvents);
-      } else if (nextWeek.getTime() < eventsState[0].start!.getTime()) {
-        recurringEvents = eventsState.map((event: Event) => {
-          return {
-            ...event,
-            start: subDays(event.start!, 7),
-            end: subDays(event.end!, 7)
-          };
-        });
-        setEventsState(recurringEvents);
-      }
-    }
-  };
+  // const handleRecurringEvents = (nextWeek: Date) => {
+  //   console.log("Nawet tutaj");
+  //   let recurringEvents: Event[] = [];
+  //   if (eventsState.length > 0) {
+  //     if (nextWeek.getTime() > eventsState[0].start!.getTime()) {
+  //       recurringEvents = eventsState.map((event: Event) => {
+  //         return {
+  //           ...event,
+  //           start: addDays(event.start!, 7),
+  //           end: addDays(event.end!, 7)
+  //         };
+  //       });
+  //       setEventsState(recurringEvents);
+  //     } else if (nextWeek.getTime() < eventsState[0].start!.getTime()) {
+  //       recurringEvents = eventsState.map((event: Event) => {
+  //         return {
+  //           ...event,
+  //           start: subDays(event.start!, 7),
+  //           end: subDays(event.end!, 7)
+  //         };
+  //       });
+  //       setEventsState(recurringEvents);
+  //     }
+  //   }
+  // };
 
   const isSelectionOverlaping = (start: Date | string, end: Date | string) => {
     return eventsState.find((event: Event) => {
@@ -71,11 +72,9 @@ const WorkScheduleCalendar: React.FC<WorkingScheduleCalendarPropsT> = ({
         defaultView={"week"}
         selectable={true}
         views={{
-          month: false,
           week: true,
           work_week: true
         }}
-        drilldownView="week"
         onSelecting={(range) => {
           if (isSelectionOverlaping(range.start, range.end)) {
             return false;
@@ -102,7 +101,7 @@ const WorkScheduleCalendar: React.FC<WorkingScheduleCalendarPropsT> = ({
             setEventsState(eventsState.filter((e) => e !== event));
           }
         }}
-        onNavigate={(nextWeekDate) => handleRecurringEvents(nextWeekDate)}
+        // onNavigate={(nextWeekDate) => handleRecurringEvents(nextWeekDate)}
       />
     </>
   );
