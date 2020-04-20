56abd1623c3a6b0082810b5f86b229bc42e4b58d
Mateusz Wende <matewen129@student.polsl.pl>
2020-04-20 13:08:44 +0200

    Fix type import

--- front-end/src/components/VacationsForm.tsx
+++ front-end/src/components/VacationsForm.tsx
@@ -2,20 +2,16 @@ import React, { useState, useRef, useEffect } from "react";
 import { Row, Col, FormGroup, FormLabel, Form } from "react-bootstrap";
 import { addDays, subDays } from "date-fns/esm";
 import { UserT } from "../state/ducks/user/types";
-import { VacationsCategoryT } from "../state/ducks/vacations/types";
+import {
+  VacationsCategoryT,
+  VacationsFormDataT,
+} from "../state/ducks/vacations/types";
 import FormikWithRef from "./FormikWithRef";
 import * as Yup from "yup";
 import { FormikProps } from "formik";
 import { DatePickerField } from "./DatePickerField";
 import { LeftVacationsDaysT } from "../state/ducks/selected-worker/types";
 import LoadingButton from "./LoadingButton";
-
-  export type VacationsFormDataT = {
-    fromDate: Date;
-    toDate: Date;
-    categoryId: number;
-    substitutionId: string;
-  };
 
 type VacationsFormPropsT = {
   readonly categories?: VacationsCategoryT[];
